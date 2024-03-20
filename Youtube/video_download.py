9d4e5663e6b724b555fdec3875dba920261a775b
Garvitjoshi1 <garvitjoshi46@gmail.com>
2024-03-20 11:21:59 +0530

    Updated

--- Youtube/video_download.py
+++ Youtube/video_download.py
@@ -1,5 +1,5 @@
 from pytube import YouTube
-video_url = 'https://www.youtube.com/watch?v=Pl3NBhMgDZs'
+video_url = 'YOUTUBE VIDEO LINK'
 yt = YouTube(video_url)
 video_stream = yt.streams.get_highest_resolution()
 video_stream.download()
